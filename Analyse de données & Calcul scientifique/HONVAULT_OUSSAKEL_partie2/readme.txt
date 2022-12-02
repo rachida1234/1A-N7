5d10ffdccfcd2fda0839dae8b26ef0f54ad2dc54
Oussakel rachida <106442952+rachida1234@users.noreply.github.com>
2022-12-02 22:46:49 +0100

    Add files via upload

---
+++ Analyse de données & Calcul scientifique/HONVAULT_OUSSAKEL_partie2/readme.txt
@@ -0,0 +1,14 @@
+Here are explained the roles of each "test_XXXX" files.
+
+Native:
+-- test_v11 : to use power_v11.
+-- test_v12 : to use power_v12.
+-- test_v0v1 : to use subspace_iter_v0 or _v1.
+
+Personnal:
+-- test_Q10iter : to process and display (using subspace_iter_v2) the processing time for each type
+of matrix as a function of p. (p is a vector of the values of p to test).
+
+-- test_Q15v0 : ___ same using subspace_iter_v0
+-- test_Q15v1 : ___ same using subspace_iter_v1
+-- test_Q15v3 : ___ same using subspace_iter_v3
---
+++ Analyse de données & Calcul scientifique/HONVAULT_OUSSAKEL_partie2/test_Q15v1.m
@@ -0,0 +1,83 @@
+
+close all;
+clear all;
+format long;
+%% Variation de nombre d'itération en fonction de p :
+%%%%%%%%%%%%
+% PARAMÈTRES
+%%%%%%%%%%%%
+
+% taille de la matrice symétrique
+n = 200; 
+
+% type de la matrice (voir matgen_csad)
+% imat == 1 valeurs propres D(i) = i
+% imat == 2 valeurs propres D(i) = random(1/cond, 1) avec leur logarithmes
+%                                  uniformément répartie, cond = 1e10
+% imat == 3 valeurs propres D(i) = cond**(-(i-1)/(n-1)) avec cond = 1e5
+% imat == 4 valeurs propres D(i) = 1 - ((i-1)/(n-1))*(1 - 1/cond) avec cond = 1e2
+
+
+for imat = 1:4
+    
+    % on génère la matrice (1) ou on lit dans un fichier (0)
+    % si vous avez déjà généré la matrice d'une certaine taille et d'un type donné
+    % vous pouvez mettre cette valeur à 0
+    genere = 1;
+    
+    % tolérance
+    eps = 1e-8;
+    % nombre d'itérations max pour atteindre la convergence
+    maxit = 10000;
+    % nombre maximum de couples propres calculés
+    m = 20;
+    percentage = 0.4;
+    % Génération d'une matrice rectangulaire aléatoire symétrique définie
+    % positive A de taille (n x n)
+    % A matrice
+    % D ses valeurs propres
+    fprintf('\n******* création de la matrice ******\n');
+    t_v =  cputime;
+    [A, D, ~] = matgen_csad(imat,n);
+    t_v = cputime-t_v;
+    fprintf('\nTemps de création de la matrice = %0.3e\n',t_v)
+    save(['A_' num2str(n) '_' num2str(imat)], 'A', 'D', 'imat', 'n');
+
+    % p le vecteurs des valeurs de p à tester pour A^p
+    p=1:1:10;
+
+    % Nombre d'itérations nécessaires 
+    nbr_IT = zeros(1,length(p));
+
+    % temps de calcul
+    temps_IT = zeros(1,length(p));
+
+    for i = 1:length(p)
+
+        % Retenir le temps courant
+        t_v1 = cputime;   
+
+        % Calculer les valeurs propres nécessaires avec subspace_iter_v0
+        [ W, V, n_ev, it, itv, flag ] = subspace_iter_v1( A, m, percentage, eps, maxit);
+
+        % Comparer le temps courant au temps retenu
+        t_v1 = cputime-t_v1;
+        temps_IT(i)=t_v1;
+
+        % Récupérer le nombre d'itérations qui ont été nécessaires pour
+        % cette valeur de p
+        nbr_IT(i)=it;
+
+    end
+    
+    % Afficher
+    figure(1);plot(p,temps_IT,'-');
+    hold on;
+
+end
+
+% Légende de la courbe
+xlabel('p');
+ylabel("Processing time (s)");
+legend('Mat1','Mat2','Mat3','Mat4');
+title("Variation of processing time as a function of p");
